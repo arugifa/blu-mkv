245dbaba75905755b0e780414a8a4f426423bb1f
Alexandre Figura <git@alexandre.figura.live>
2015-11-15 16:58:25 +0100

    Add utils to mount/unmount bluray disc images

---
+++ blu_mkv/utils.py
@@ -0,0 +1,26 @@
+import subprocess
+
+
+def mount_disk_image(image_path, mount_point):
+    """Mount disk image with sudo permissions.
+
+    :param image_path: disk image's path.
+    :param mount_point: directory's path where to mount the disk image.
+    """
+    subprocess.check_call([
+        'sudo',
+        'mount',
+        '-o loop',
+        image_path,
+        mount_point])
+
+
+def umount_disk_image(mount_point):
+    """Umount file system with sudo permissions.
+
+    :param mount_point: directory's path where the file system is mounted.
+    """
+    subprocess.check_call([
+        'sudo',
+        'umount',
+        mount_point])
---
+++ tests/unit_tests/test_utils.py
@@ -0,0 +1,24 @@
+import subprocess
+
+from blu_mkv import utils
+
+
+def test_mount_disk_image(mock):
+    image_path = '/home/user/downloads/bluray.iso'
+    mount_point = '/tmp/user/bluray'
+
+    mock.patch.object(subprocess, 'check_call')
+    utils.mount_disk_image(image_path, mount_point)
+
+    subprocess.check_call.assert_called_once_with(
+        ['sudo', 'mount', '-o loop', image_path, mount_point])
+
+
+def test_umount_disk_image(mock):
+    mount_point = '/tmp/user/bluray'
+
+    mock.patch.object(subprocess, 'check_call')
+    utils.umount_disk_image(mount_point)
+
+    subprocess.check_call.assert_called_once_with(
+        ['sudo', 'umount', mount_point])

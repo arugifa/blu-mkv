01a27146513988496a235e1778f195c2bafef7b3
Alexandre Figura <git@alexandre.figura.live>
2015-11-22 18:30:48 +0100

    Update the Ffprobe stub controller

--- blu_mkv/test.py
+++ blu_mkv/test.py
@@ -12,3 +12,34 @@ class StubFfprobeController(AbstractFfprobeController):
             [bluray @ 0x555da3c70e60] playlist 00419.mpls (2:23:11)
             [bluray @ 0x555da3c70e60] selected 00419.mpls
             """)
+
+    def get_all_streams_of_bluray_playlist_as_json(
+            self, disc_path, playlid_id):
+        return {
+            'streams': [
+                {'index': 0,
+                 'codec_type': 'video'},
+                {'index': 1,
+                 'codec_type': 'audio'},
+                {'index': 2,
+                 'codec_type': 'audio'},
+                {'index': 3,
+                 'codec_type': 'subtitle'},
+                {'index': 4,
+                 'codec_type': 'subtitle'},
+                {'index': 5,
+                 'codec_type': 'subtitle'}]}
+
+    def get_bluray_playlist_subtitles_with_frames_count_as_json(
+            self, disc_path, playlist_id):
+        return {
+            'streams': [
+                {'index': 3,
+                 'codec_type': 'subtitle',
+                 'nb_read_frames': '2810'},
+                {'index': 4,
+                 'codec_type': 'subtitle',
+                 'nb_read_frames': '18'},
+                {'index': 5,
+                 'codec_type': 'subtitle',
+                 'nb_read_frames': '2680'}]}

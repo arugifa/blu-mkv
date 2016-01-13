0ae3c5d8790caf6f87fa594cd17a7414f16228d7
Alexandre Figura <git@alexandre.figura.live>
2016-01-13 18:51:17 +0100

    Add markers for integration tests
    This allows to execute integration tests for checking interface of specific (not all) controllers.

--- tests/integration_tests/test_bluray.py
+++ tests/integration_tests/test_bluray.py
@@ -1,14 +1,21 @@
+import pytest
+
+
 class TestBlurayAnalyzer:
+    @pytest.mark.ffprobe
     def test_get_playlists(self, bluray_analyzer, bluray_path):
         playlists = bluray_analyzer.get_playlists(bluray_path)
         assert len(playlists) > 0
 
+    @pytest.mark.ffprobe
+    @pytest.mark.mkvmerge
     def test_get_playlist_tracks(self, bluray_analyzer, bluray_path):
-        default_playlist = bluray_analyzer.ffprobe \
-                           .get_default_bluray_playlist_number(bluray_path)
+        default_playlist = (
+            bluray_analyzer.ffprobe
+            .get_default_bluray_playlist_number(bluray_path))
 
-        tracks = bluray_analyzer \
-                 .get_playlist_tracks(bluray_path, default_playlist)
+        tracks =\
+            bluray_analyzer.get_playlist_tracks(bluray_path, default_playlist)
 
         for track_type in ['video', 'audio', 'subtitle']:
             assert len(tracks[track_type]) > 0
--- tests/integration_tests/test_ffprobe.py
+++ tests/integration_tests/test_ffprobe.py
@@ -1,3 +1,8 @@
+import pytest
+
+pytestmark = pytest.mark.ffprobe
+
+
 class TestFfprobeController:
     def test_get_default_bluray_playlist_number(self, ffprobe, bluray_path):
         default_playlist = ffprobe \
